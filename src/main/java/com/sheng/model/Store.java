893a8d6ef011b1a61c35d88729e6ad6d6c9fef32
郑志胜 <2317286334@qq.com>
2017-10-13 23:20:05 +0800

    feat:添加mock

---
+++ src/main/java/com/sheng/model/Store.java
@@ -0,0 +1,17 @@
+package com.sheng.model;
+
+import lombok.Data;
+
+/**
+ * 存货
+ * <p>
+ * Created by zhengzs on 2017/10/13.
+ */
+@Data
+public class Store {
+    private double price;
+
+    public double getPrice(String name){
+        return 12;
+    }
+}
