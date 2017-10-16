1863de3aed091e8f7a955bcef0701eeefa1b4f09
郑志胜 <2317286334@qq.com>
2017-10-16 22:49:13 +0800

    feat:添加组织userDO

---
+++ src/main/java/com/sheng/model/Item.java
@@ -0,0 +1,29 @@
+package com.sheng.model;
+
+/**
+ * 商品
+ * <p>
+ * Created by zhengzs on 2017/10/13.
+ */
+public class Item {
+    private String name;
+    private int quantity;
+
+    public Item(String name, int quantity) {
+        super();
+        this.name = name;
+        this.quantity = quantity;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
