555246337b4bb49908b66441904477fcb6646c1a
郑志胜 <2317286334@qq.com>
2017-10-18 22:37:49 +0800

    feat:添加mapper

---
+++ src/main/java/com/sheng/mapper/ResourceMapper.java
@@ -0,0 +1,13 @@
+package com.sheng.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.sheng.model.Resource;
+
+/**
+ *
+ * Resource 表数据库控制层接口
+ *
+ */
+public interface ResourceMapper extends BaseMapper<Resource> {
+
+}
---
+++ src/main/java/com/sheng/mapper/RoleMapper.java
@@ -0,0 +1,22 @@
+package com.sheng.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.sheng.model.Resource;
+import com.sheng.model.Role;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Role 表数据库控制层接口
+ */
+public interface RoleMapper extends BaseMapper<Role> {
+
+    List<Long> selectResourceIdListByRoleId(@Param("id") Long id);
+
+    List<Resource> selectResourceListByRoleIdList(@Param("list") List<Long> list);
+
+    List<Map<Long, String>> selectResourceListByRoleId(@Param("id") Long id);
+
+}
